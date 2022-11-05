53cfebfea053639f5df0fe6a3b73d2107c2b3e3d
ChristinaAlferieva <C19401882@mytudublin.ie>
2022-11-05 16:27:41 +0000

    Validated all the inputs

--- problem-1/site.js
+++ problem-1/site.js
@@ -17,3 +17,29 @@ var contact = [];
 error.innerHTML = "";
 noResult.innerHTML = "";
 
+function validateInput(name, mobile, email){
+    if(name.length == 0){
+        error.innerHTML = "Please enter the name!";
+        return false;
+    }
+    else if(mobile.length == 0){
+        error.innerHTML = "Please enter the mobile number!";
+        return false;
+    }
+    else if(email.length == 0){
+        error.innerHTML = "Please enter the email address!";
+        return false;
+    }
+    else if(!name.match(letter)){
+        error.innerHTML = "The name should contain only letters!";
+        return false;
+    }
+    else if(mobile.length !== 10){
+        error.innerHTML = "The mobile number must be 10 numbers long!";
+        return false;
+    }
+    else{
+        error.innerHTML = "";
+        return true;
+    }
+}
