32eb2a482bc228479e3d2f8887eb8d98ccd7302f
ChristinaAlferieva <C19401882@mytudublin.ie>
2022-11-06 13:29:01 +0000

    Got the data for User Profile to show

--- problem-3/site.js
+++ problem-3/site.js
@@ -1,11 +1,25 @@
 var form = document.getElementById("searchForm")
 
 form.addEventListener('submit', function(getData){
-    getData.preventDefault()
+    getData.preventDefault();
 
-    var search = document.getElementById("search").value
+    var search = document.getElementById("search").value;
 
     fetch("https://api.github.com/users/"+search)
     .then((response) => response.json())
-    .then((json) => console.log(json))
+    .then((json) => {
+        console.log(json)
+
+        document.getElementById("image").innerHTML = `<img src="${json.avatar_url}"/> `;
+        document.getElementById("name").innerHTML = `<label><b>Name:    </b></label>${json.name}`;
+        document.getElementById("username").innerHTML = `<label><b>Username:    </b></label>${json.login}`;
+        document.getElementById("email").innerHTML = `<label><b>Email:    </b></label>${json.email}`;
+        document.getElementById("location").innerHTML = `<label><b>Location:    </b></label>${json.location}`;
+        document.getElementById("gists").innerHTML = `<label><b>Number of Gists:    </b></label>${json.public_gists}`;
+
+        document.getElementById("userRepos").innerHTML = `${json.repos_url.description}`;
+                
+    })
+
+
 })
