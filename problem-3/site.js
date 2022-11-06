4b1fdc56a204ad3ee3fa2c3fddb15a9725a5df62
ChristinaAlferieva <C19401882@mytudublin.ie>
2022-11-06 15:59:14 +0000

    Completed the CSS file

--- problem-3/site.js
+++ problem-3/site.js
@@ -11,13 +11,21 @@ form.addEventListener('submit', function(getData){
         console.log(json)
 
         document.getElementById("image").innerHTML = `<img src="${json.avatar_url}"/> `;
-        document.getElementById("name").innerHTML = `<label><b>Name:    </b></label>${json.name}`;
-        document.getElementById("username").innerHTML = `<label><b>Username:    </b></label>${json.login}`;
-        document.getElementById("email").innerHTML = `<label><b>Email:    </b></label>${json.email}`;
-        document.getElementById("location").innerHTML = `<label><b>Location:    </b></label>${json.location}`;
-        document.getElementById("gists").innerHTML = `<label><b>Number of Gists:    </b></label>${json.public_gists}`;
+        document.getElementById("name").innerHTML = `<label>Name:    </label>${json.name}`;
+        document.getElementById("username").innerHTML = `<label>Username:    </label>${json.login}`;
+        document.getElementById("email").innerHTML = `<label>Email:    </label>${json.email}`;
+        document.getElementById("location").innerHTML = `<label>Location:    </label>${json.location}`;
+        document.getElementById("gists").innerHTML = `<label>Number of Gists:    </label>${json.public_gists}`;
+        
+        const reposUrl = `${json.repos_url}`;
 
-        document.getElementById("userRepos").innerHTML = `${json.repos_url.description}`;
+        fetch(reposUrl)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json)
+
+            document.getElementById("userRepos").innerHTML = `${json.full_name}`;
+        })
                 
     })
 
